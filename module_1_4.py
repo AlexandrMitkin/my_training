df34e81309bd79d75baef8d527552a06655f16bc
AlexandrMitkin <ferma1800@yandex.ru>
2024-09-13 22:33:22 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+﻿from typing import SupportsIndex
+my_string=input("input the text with пробелами ")
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
