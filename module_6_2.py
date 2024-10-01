f4c0ffdd9235fbdcd1383f2a980051c1a46ba56f
AlexandrMitkin <ferma1800@yandex.ru>
2024-10-01 21:02:02 +0300

    Add files via upload

--- module_6_2.py
+++ module_6_2.py
@@ -1,36 +1,40 @@
 class Vehicle:
     __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
-    def __init__(self, owner1, model1, engine_power1, color1):
+    def __init__(self, owner1, model1, color1, engine_power1):
         self.owner = owner1
-        self.__model = "model_a1"
-        self.__engine_power = "power_b1"
-        self.__color = "blue"
-        self._model = self.__model
-        self._engine_power = self.__engine_power
-        self._color = self.__color
+        self.__model = model1
+        self.__engine_power = engine_power1
+        self.__color = color1
+        # self.__model = "model_a1"
+        # self.__engine_power = "power_b1"
+        # self.__color = "blue"
+        # self._model = self.__model
+        # self._engine_power = self.__engine_power
+        # self._color = self.__color
 
     def get_model(self):
-        return f"Модель: {self._model}"
+        return f"Модель: {self.__model}"
 
     def get_horsepower(self):
-        return f"Мощность двигателя: {self._engine_power}"
+        return f"Мощность двигателя: {self.__engine_power}"
 
     def get_color(self):
-        return f"Цвет: {self._color}"
+        return f"Цвет: {self.__color}"
 
     def print_info(self):
         print("\033[34m" + self.get_model())
         print(self.get_horsepower())
         print(self.get_color())
-        print(f"Владелец: {self.owner}")
+        print(f"Владелец: \033[32m{self.owner}")
 
     def set_color(self, new_color):
         r = 0
+
         for i in self.__COLOR_VARIANTS:
             if i.upper() == new_color.upper():
                 r = 1
-                self._color = "\033[32m" + new_color + "\033[34m"
+                self.__color = "\033[32m" + new_color + "\033[34m"
         if r == 0:
             self.owner = "\033[32m" + self.owner
             print(f"\033[31mНельзя сменить цвет на {new_color}")
@@ -40,10 +44,11 @@ class Sedan(Vehicle):
     __PASSENGERS_LIMIT = 5
 
     def __init__(self, owner1, model1, color1, engine_power1):
-        self.owner = owner1
-        self._model = model1
-        self._engine_power = engine_power1
-        self._color = color1
+        # self.owner = owner1
+        super().__init__(owner1, model1, color1, engine_power1)
+        # self._model = model1
+        # self._engine_power = engine_power1
+        # self._color = color1
 
 
 # Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
@@ -55,7 +60,7 @@ vehicle1.print_info()
 # Меняем свойства (в т.ч. вызывая методы)
 vehicle1.set_color('Pink')
 vehicle1.set_color('BLACK')
-# vehicle1.owner = 'Vasyok'
+vehicle1.owner = 'Vasyok'
 
 # Проверяем что поменялось
 vehicle1.print_info()
