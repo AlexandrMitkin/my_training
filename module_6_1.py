eb3c6dc39ec614e64d72f2c32c4111f4fd90a8fe
AlexandrMitkin <ferma1800@yandex.ru>
2024-10-02 00:08:28 +0300

    Add files via upload

--- module_6_1.py
+++ module_6_1.py
@@ -7,7 +7,8 @@ class Animal:
 
 
 class Plant:
-    edible = False
+    edible_M = False
+    edible_P = False
 
     def __init__(self, name1):
         self.name = name1
@@ -20,9 +21,20 @@ class Mammal(Animal):
         self.alive = Animal.alive
 
     def eat(self, food1):
-        if isinstance(food1, Plant):
-            print(f"{self.name} съел {food1.name}")
-            self.fed = True
+        if isinstance(food1, Fruit):
+            if Fruit.edible_M == True:
+                print(f"{self.name} съел {food1.name}")
+                self.fed = True
+            else:
+                print(f"{self.name} не стал есть {food1.name}")
+                self.alive = False
+        elif isinstance(food1, Flower):
+            if Flower.edible_M == True:
+                print(f"{self.name} съел {food1.name}")
+                self.fed = True
+            else:
+                print(f"{self.name} не стал есть {food1.name}")
+                self.alive = False
 
 
 class Predator(Animal):
@@ -32,27 +44,42 @@ class Predator(Animal):
         self.alive = Animal.alive
 
     def eat(self, food1):
-        if isinstance(food1, Plant):
-            print(f"{self.name} не стал есть {food1.name}")
-            self.alive = False
+        if isinstance(food1, Fruit):
+            if Fruit.edible_P == True:
+                print(f"{self.name} съел {food1.name}")
+                self.fed = True
+            else:
+                print(f"{self.name} не стал есть {food1.name}")
+                self.alive = False
+        elif isinstance(food1, Flower):
+            if Flower.edible_P == True:
+                print(f"{self.name} съел {food1.name}")
+                self.fed = True
+            else:
+                print(f"{self.name} не стал есть {food1.name}")
+                self.alive = False
 
 
 class Flower(Plant):
-    # edible1 = False
-    edible = Plant.edible
+    edible_M = Plant.edible_M
+    edible_P = Plant.edible_P
 
-    def __init__(self, name1):
-        # global edible1
+    def __init__(self, name1, edible_M1=Plant.edible_M, edible_P1=Plant.edible_P):
         self.name = name1
-        self.edible1 = Plant.edible
+        Flower.edible_M = edible_M1
+        Flower.edible_P = edible_P1
 
 
 class Fruit(Plant):
-    edible = True
+    edible_M = Plant.edible_M
+    edible_P = Plant.edible_P
 
-    def __init__(self, name1):
+    def __init__(self, name1, edible_M1=True, edible_P1=Plant.edible_P):
         self.name = name1
-        # self.edible = True
+        # self.edible_M = edible_M1
+        # self.edible_P = edible_P1
+        Fruit.edible_M = edible_M1
+        Fruit.edible_P = edible_P1
 
 
 a1 = Predator('Волк с Уолл-Стрит')
