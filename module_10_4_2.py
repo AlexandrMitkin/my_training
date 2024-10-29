b78e58c978dc0b7e23d34f880355378325c21ec1
AlexandrMitkin <ferma1800@yandex.ru>
2024-10-29 22:52:30 +0300

    Add files via upload

--- module_10_4_2.py
+++ module_10_4_2.py
@@ -10,82 +10,81 @@ class Table:
         self.number = table_number
         self.guest = table_guest
 
+
 class Guest(threading.Thread):
     def __init__(self, guest_name):
         threading.Thread.__init__(self)
         self.name = guest_name
 
     def run(self):
-        #print(f"гость {self.name} спит ")
+        # print(f"гость {self.name} спит ")
         sleep(random.randint(3, 10))
-        #print(f"гость {self.name} проснулся ")
+        # print(f"гость {self.name} проснулся ")
+
 
 class Cafe:
     def __init__(self, *tables1):
         self.tables = tables1
-        self.queue=queue.Queue()
+        self.queue = queue.Queue()
 
     def guest_arrival(self, *guests):
         l = len(guests)
-        i1=0
+        i1 = 0
         for i in self.tables:
             if i.guest == None and i1 < l:
                 g2 = guests[i1].name
                 i.guest = g2
                 print(f"{g2} сел(-а) за стол номер {i.number}")
                 guests[i1].start()
-            i1 +=1
-        #если гости усажены не все, то добавляем в очередь
-        if i1<l:
+            i1 += 1
+        # если гости усажены не все, то добавляем в очередь
+        if i1 < l:
             for j in range(i1, l):
                 g2 = guests[j]
                 print(f"{g2.name} в очереди")
                 self.queue.put(g2)
 
     def discuss_guests(self):
-        #определяем, что не все столы свободны
+        # определяем, что не все столы свободны
         r = 0
         for i in self.tables:
             if i.guest != None:
                 r = 1
                 break
-        #если очередь не пуста или есть занятые столы, то обслуживаем
+        # если очередь не пуста или есть занятые столы, то обслуживаем
         while self.queue.empty() == False or r == 1:
-            r1=0
-            k1=0
-            #ищем стол, где гость покушал
-            #r1 - флаг того, все столы заняты
-            #пока столы заняты, ждём и смотрим за гостями за столами
-            while r1==0:
+            r1 = 0
+            k1 = 0
+            # ищем стол, где гость покушал
+            # r1 - флаг того, все столы заняты
+            # пока столы заняты, ждём и смотрим за гостями за столами
+            while r1 == 0:
                 for i in self.tables:
-                    #определяем имя гостя за столом
+                    # определяем имя гостя за столом
                     if i.guest != None:
-                        #определяем поток гостя по его имени
+                        # определяем поток гостя по его имени
                         for j in guests:
                             if j.name == i.guest:
-                                #смотрим, покушал ли гость?
+                                # смотрим, покушал ли гость?
                                 if j.is_alive() == False:
                                     print(f"{i.guest} покушал(-а) и ушёл(ушла)")
                                     print(f"Стол номер {i.number} свободен")
                                     i.guest = None
-                                    #запоминаем номер стола и запоминаем, что освободили
-                                    k1=i.number-1
-                                    r1=1
-                                    r=0
+                                    # запоминаем номер стола и запоминаем, что освободили
+                                    k1 = i.number - 1
+                                    r1 = 1
+                                    r = 0
                                 break
-            #стол освободился. Если очередь не пуста, то сажаем за стол следующего
+            # стол освободился. Если очередь не пуста, то сажаем за стол следующего
             if self.queue.empty() == False:
                 g2 = self.queue.get()
                 # print("k1= ", k1)
                 # print("g2.name= ", g2.name)
                 tables[k1].guest = g2.name
-                print(f"{g2.name} вышел(-ла) из очереди и сел(-а) за стол номер {k1+1}")
-                #запоминаем, что не все столы пусты
-                r=1
+                print(f"{g2.name} вышел(-ла) из очереди и сел(-а) за стол номер {k1 + 1}")
+                # запоминаем, что не все столы пусты
+                r = 1
                 g2.start()
-
-
-
 
 
 # Создание столов
@@ -106,4 +105,3 @@ cafe.guest_arrival(*guests)
 
 # Обслуживание гостей
 cafe.discuss_guests()
-
