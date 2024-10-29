c5ab24b72a773198319aad023d9317194bd3eed6
AlexandrMitkin <ferma1800@yandex.ru>
2024-10-29 23:06:43 +0300

    Add files via upload

--- module_10_4_2.py
+++ module_10_4_2.py
@@ -7,14 +7,14 @@ import threading
 
 class Table:
     def __init__(self, table_number, table_guest=None):
-        self.number = table_number
-        self.guest = table_guest
+        self.table_number = table_number
+        self.table_guest = table_guest
 
 
 class Guest(threading.Thread):
     def __init__(self, guest_name):
         threading.Thread.__init__(self)
-        self.name = guest_name
+        self.guest_name = guest_name
 
     def run(self):
         # print(f"гость {self.name} спит ")
@@ -31,24 +31,24 @@ class Cafe:
         l = len(guests)
         i1 = 0
         for i in self.tables:
-            if i.guest == None and i1 < l:
-                g2 = guests[i1].name
-                i.guest = g2
-                print(f"{g2} сел(-а) за стол номер {i.number}")
+            if i.table_guest == None and i1 < l:
+                g2 = guests[i1].guest_name
+                i.table_guest = g2
+                print(f"{g2} сел(-а) за стол номер {i.table_number}")
                 guests[i1].start()
             i1 += 1
         # если гости усажены не все, то добавляем в очередь
         if i1 < l:
             for j in range(i1, l):
                 g2 = guests[j]
-                print(f"{g2.name} в очереди")
+                print(f"{g2.guest_name} в очереди")
                 self.queue.put(g2)
 
     def discuss_guests(self):
         # определяем, что не все столы свободны
         r = 0
         for i in self.tables:
-            if i.guest != None:
+            if i.table_guest != None:
                 r = 1
                 break
         # если очередь не пуста или есть занятые столы, то обслуживаем
@@ -61,17 +61,17 @@ class Cafe:
             while r1 == 0:
                 for i in self.tables:
                     # определяем имя гостя за столом
-                    if i.guest != None:
+                    if i.table_guest != None:
                         # определяем поток гостя по его имени
                         for j in guests:
-                            if j.name == i.guest:
+                            if j.guest_name == i.table_guest:
                                 # смотрим, покушал ли гость?
                                 if j.is_alive() == False:
-                                    print(f"{i.guest} покушал(-а) и ушёл(ушла)")
-                                    print(f"Стол номер {i.number} свободен")
+                                    print(f"{i.table_guest} покушал(-а) и ушёл(ушла)")
+                                    print(f"Стол номер {i.table_number} свободен")
                                     i.guest = None
                                     # запоминаем номер стола и запоминаем, что освободили
-                                    k1 = i.number - 1
+                                    k1 = i.table_number - 1
                                     r1 = 1
                                     r = 0
                                 break
@@ -80,7 +80,7 @@ class Cafe:
                 g2 = self.queue.get()
                 # print("k1= ", k1)
                 # print("g2.name= ", g2.name)
-                tables[k1].guest = g2.name
+                tables[k1].table_guest = g2.guest_name
                 print(f"{g2.name} вышел(-ла) из очереди и сел(-а) за стол номер {k1 + 1}")
                 # запоминаем, что не все столы пусты
                 r = 1
