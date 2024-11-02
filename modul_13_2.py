8ed224e30e024bc29bc6f9ca1365d1227ef6a989
AlexandrMitkin <ferma1800@yandex.ru>
2024-11-02 23:27:10 +0300

    Add files via upload

--- modul_13_2.py
+++ modul_13_2.py
@@ -2,10 +2,9 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api=""
-bot=Bot(token=api)
-dp=Dispatcher(bot, storage=MemoryStorage())
-
+api = ""
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 
 # @dp.message_handler(text=["python_pancake", "ff"])
@@ -16,9 +15,11 @@ dp=Dispatcher(bot, storage=MemoryStorage())
 async def comand_message(message):
     print("Привет! Я бот помогающий твоему здоровью.")
 
+
 @dp.message_handler()
 async def all_message(message):
     print("Введите команду /start, чтобы начать общение.")
+
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
